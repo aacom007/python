00871df752a3a9bf1c0074bde52b41f5a020a366
aacom007 <akshayan@usc.edu>
2016-04-02 09:56:34 -0700

    sql script

---
+++ scripts.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE `nbc1` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE nbc1
+
+CREATE TABLE `nbcdemo` (
+  `keyName` varchar(200) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
